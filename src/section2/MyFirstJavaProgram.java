7e3b55abbe225e2a6012b1eaab4e37f959e97943
League <league@WTS9.local>
2018-02-16 14:56:28 -0800

    day 1

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,18 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot Bob = new Robot ();
+Bob.setSpeed(500);
+Bob.penDown();		
+Bob.move (200);	
+Bob.turn (90);
+Bob.move(200);
+Bob.	turn(90);
+Bob.move(200);
+Bob.turn(90);
+Bob.move(200);
+Bob.hide	}	
 	
-		
-		
 	}
-}
+
+
--- src/section3/StephenHawking.java
+++ src/section3/StephenHawking.java
@@ -7,11 +7,14 @@ import com.sun.speech.freetts.VoiceManager;
 public class StephenHawking {
 	
 	// 1. make a main method and put steps 2, 3 & 4 inside it
+	public static void main(String[] args) {
+		
 	
 	// 4. Use a for loop to repeat steps #2 and #3, a lot of times
-	
+
 	// 2. ask the user for a sentence
-	
+	speak("Hi Bob");
+	}
 	// 3. call the speak method below and send it the sentence
 
 	
@@ -33,4 +36,10 @@ public class StephenHawking {
 		 }
 	}
 
+}
+
+	private static void speak() {
+		// TODO Auto-generated method stub
+		
+	}
 }
